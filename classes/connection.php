f01640e67a5ad8ba99cba09a7c6ddae314774333
abenfield <abenfield@protonmail.com>
2020-04-06 17:34:29 -0500

    Added Admin Section

---
+++ classes/connection.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "209.106.201.103";
+$username = "dbstudent2"; //Your database username
+$password = "icybone58"; //Your database password
+$dbname = "group3"; //Your database
+
+$db = new mysqli($hostname, $username, $password, $dbname);
+?>
+
